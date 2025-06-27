163872871e465e9b4dc93260738fb2b6f067bdcb
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-06-28 00:07:31 +0900

    吹き出しcss作成

--- front/src/features/actual/conversationLog/ConversationLogPage.jsx
+++ front/src/features/actual/conversationLog/ConversationLogPage.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Box, Container, Flex } from '@yamada-ui/react';
+import './ContextFrame.css'
+
+const yourContext = [
+  '今日中にやらないといけない仕事が残っているんですけど、、、今日はゲームの発売日なので定時で帰ります。',
+  '遅れた分は来週頑張りますし。今日も定時まで頑張りますので、今日は定時で帰ります。',
+];
+
+const myContext = ['え？ちょっと待って、納期が今日までなんだよね？遅れたら、責任取れるの？'];
+
+const usertype = {
+  answer1: '自分の成長を実感できたとき',
+  answer2: '部下の挑戦を応援してくれる人',
+  answer3: '自分の中で納得できること',
+  answer4: 'どちらも全力。仕事が充実すればプライベートも充実する',
+  answer5: '助け合いやチームワークが強い',
+  character: '',
+  created_at: '2025-06-27T10:35:25.232Z',
+  hash: 'aaa',
+  id: 1,
+  nickname: 'ゆうたろう',
+  org_code: '99999',
+  salt: '111',
+  search_id: 1001,
+};
 
 function ConversationLogPage() {
   return (
-    <div>ConversationLogPage</div>
-  )
+    <Container>
+      ConversationLogPage
+      <Box>
+        <Box>
+          <Avatar
+            name={usertype.nickname}
+            size="sm"
+          />
+          {usertype.nickname}
+        </Box>
+        <Flex direction="row" className='message-bubble'>
+          <Box
+          className='message-bubble--other'
+            bg="gray.50"
+            margin="3"
+            rounded="lg"
+            padding="2"
+            width="80%">
+            {yourContext[0]}
+          </Box>
+        </Flex>
+        <Flex
+        className='message-bubble'
+          direction="row"
+          justify="end">
+          <Box
+          className='message-bubble--mine'
+            bg="green.300"
+            margin="3"
+            rounded="lg"
+            padding="2"
+            width="80%">
+            {myContext[0]}
+          </Box>
+        </Flex>
+      </Box>
+    </Container>
+  );
 }
 
-export default ConversationLogPage
+export default ConversationLogPage;
--- front/src/features/actual/listen/ListenConversation.jsx
+++ front/src/features/actual/listen/ListenConversation.jsx
@@ -1,29 +1,14 @@
-import { Box, Container } from '@yamada-ui/react';
+import { Container } from '@yamada-ui/react';
 import React from 'react';
 
-const yourContext = [
-  '今日中にやらないといけない仕事が残っているんですけど、、、今日はゲームの発売日なので定時で帰ります。',
-  '遅れた分は来週頑張りますし。今日も定時まで頑張りますので、今日は定時で帰ります。',
-];
 
-const myContext = ['え？ちょっと待って、納期が今日までなんだよね？遅れたら、責任取れるの？'];
+
 function ListenConversationPage() {
   return (
-  <Container>
-    ListenConversationPagea
-    <Box>
-      <Box>
-        アイコンyutaro
-      </Box>
-      <Box>
-        {yourContext[0]}
-      </Box>
-      <Box>
-        {myContext[0]}
-      </Box>
-    </Box>
-  </Container>
-  )
+    <Container>
+      ListenConversationPagea
+    </Container>
+  );
 }
 
 export default ListenConversationPage;
