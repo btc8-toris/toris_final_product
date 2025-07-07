b551f2e92fbab824fc73c64e803f0d7d8dadff5d
Wentz <ryo_uematsu@mail.toyota.co.jp>
2025-07-07 20:40:04 +0900

    title,favicon TacT

--- front/dev-dist/sw.js
+++ front/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s5v3uib1pqo"
+    "revision": "0.2sb9b4p4aj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
